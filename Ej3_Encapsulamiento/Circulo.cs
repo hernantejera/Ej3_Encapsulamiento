52a60e8d8353740977c758caeb84a451b0334206
hernantejera <hernanmtejera@gmail.com>
2023-07-27 15:25:50 -0300

    Agregar archivos de proyecto.

---
+++ Ej3_Encapsulamiento/Circulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej3_Encapsulamiento
+{
+    //Define una clase llamada "Círculo" con una propiedad privada "radio"
+    //y métodos públicos para obtener y establecer el radio.
+    //Asegúrate de validar que el radio no sea negativo en el método de establecimiento.
+
+    public class Circulo
+    {
+        private double Radio { get; set; }
+
+        public double EstablecerRadio(double radio)
+        {
+            while (radio > 0) ;
+            return Radio = radio;
+        }
+
+
+        internal double ObtenerRadio()
+        {
+            return Radio;
+        }
+    }
+}
---
+++ Ej3_Encapsulamiento/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ej3_Encapsulamiento
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Circulo circulo = new Circulo();
+
+            circulo.EstablecerRadio(68);
+           
+
+            double radio = circulo.ObtenerRadio();
+
+
+         Console.WriteLine($"El radio del circulo es {radio}");
+
+        }
+    }
+}
